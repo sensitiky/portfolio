a908c1f874bcb8b823cce85ba9332c0eca4d00df
sensitiky <mariomcorrea3@gmail.com>
2024-10-03 07:46:30 -0300

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Background from "@/assets/background";
+import type { Metadata } from 'next';
+import './globals.css';
+import Background from '@/assets/background';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
-  title: "Mario Correa | Portfolio",
-  description: "Developer, designer, and creator.",
+  title: 'Mario Correa | Portfolio',
+  description: 'Developer, designer, and creator.',
 };
 
 export default function RootLayout({
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-[#121212] relative">
         <Background />
+        <Analytics />
         {children}
         <ScrollIndicator />
       </body>
